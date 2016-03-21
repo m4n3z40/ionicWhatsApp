37f392329bb87ce0d4ed5bffa67c0b1b8aa1c046
Allan Baptista <allan.marques@ymail.com>
2016-03-21 02:44:25 -0300

    Initial app structure

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,56 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('ionWhatsApp', ['ionic', 'ionWhatsApp.controllers'])
 
-.run(function ($ionicPlatform) {
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('app', {
+            abstract: true,
+            templateUrl: 'templates/app.html'
+        })
+
+        .state('app.chats', {
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/tab-chats.html',
+                    controller: 'ChatsCtrl'
+                }
+            }
+        })
+        .state('app.contacts', {
+            url: '/contacts',
+            views: {
+                'tab-contacts': {
+                    templateUrl: 'templates/tab-contacts.html',
+                    controller: 'ContactsCtrl'
+                }
+            }
+        })
+        .state('app.configs', {
+            url: '/configs',
+            views: {
+                'tab-configs': {
+                    templateUrl: 'templates/tab-configs.html',
+                    controller: 'ConfigsCtrl'
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise('/chats');
+})
+
+.run(function ($ionicPlatform, $rootScope) {
+    var currentPlatform = ionic.Platform.platform();
+
+    // Make platform helper globally accessible
+    $rootScope.platform = {
+        current: currentPlatform,
+        ios: currentPlatform === 'ios',
+        android: currentPlatform === 'android'
+    };
+
     $ionicPlatform.ready(function () {
         if (window.cordova && window.cordova.plugins.Keyboard) {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -17,6 +64,7 @@ angular.module('starter', ['ionic'])
             // a much nicer keyboard experience.
             cordova.plugins.Keyboard.disableScroll(true);
         }
+
         if (window.StatusBar) {
             StatusBar.styleDefault();
         }
---
+++ www/js/controllers.js
@@ -0,0 +1,20 @@
+angular.module('ionWhatsApp.controllers', [])
+
+.controller('ChatsCtrl', function($scope) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+})
+
+.controller('ContactsCtrl', function($scope) {
+
+})
+
+.controller('ConfigsCtrl', function($scope) {
+
+});

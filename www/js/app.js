c2992f5558fd635efc8fc104d32e40e962d63cb8
Allan Baptista <allan.marques@ymail.com>
2016-04-04 20:02:59 -0300

    Adding ngCordova and contacts plugin

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('ionWhatsApp', [
+    'ngCordova',
     'ionic',
     'firebase',
     'ionWhatsApp.configs',
